d7e9e2f6084bbf92f2457c5f4b1cddd7fe7ed8e8
Sara Gonzalez <saragonzalez07sg@gmail.com>
2022-05-05 14:31:39 -0500

    clase 6 que aparece como 7 xd

---
+++ pruebas/src/main/java/clase6/oop/DB.java
@@ -0,0 +1,24 @@
+package clase6.oop;
+
+public class DB {
+
+	//no tiene atributos
+
+	//no tiene constructor
+
+	//solo tiene un metodo
+	
+	public Articulo[] consultar(String claveConsultada) {  //aqui tambien se podia poner claveBusqueda por buenas practicas pero el profe lo puso asi
+	//simular datos 
+
+			Articulo res1 = new Articulo("Figura De Marvel Avengers Titan Hero Series Blast Gear- War", 35940, "http://imagen.jpg", true, 10, true);
+			Articulo res2 = new Articulo("Figura De Accion De Marvel Avengers 6 Pulgadas Surtidas", 34930, "http://otraimagen.jpg", false, 2, false);
+
+			//??
+			//int[]
+			Articulo[] resultados = new Articulo[] {res1, res2};//length 2, 0 y 1
+			return resultados;
+		}
+
+
+	}
