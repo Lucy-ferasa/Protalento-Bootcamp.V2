6ee5a48ed7001dc594d4be28a096b26dd7c7278d
Sara Gonzalez <saragonzalez07sg@gmail.com>
2022-06-12 03:24:58 -0500

    clase 7
    -clases: encapsulamiento + sobrecarga
    -creacion app web

---
+++ pruebas/src/main/java/clase7/oop/BuscadorMainClase7.java
@@ -0,0 +1,20 @@
+package clase7.oop;
+
+import clase6.oop.Buscador;
+
+public class BuscadorMainClase7 {
+	
+	public static void main(String[] args) {
+		
+		Buscador b = new Buscador();
+
+		String claveDeBusqueda = "iron man";
+		buscador.definirClaveBusqueda(claveDeBusqueda);
+		buscador.setClaveBusqueda(claveDeBusqueda);
+
+		buscador.buscar();
+	
+	
+	}
+
+}
