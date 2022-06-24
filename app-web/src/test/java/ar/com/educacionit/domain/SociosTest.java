f12eb80de5121654a12a5506b08faa24b7c026a0
Sara Gonzalez <saragonzalez07sg@gmail.com>
2022-06-24 03:01:19 -0500

    actualizacion test

--- app-web/src/test/java/ar/com/educacionit/domain/SociosTest.java
+++ app-web/src/test/java/ar/com/educacionit/domain/SociosTest.java
@@ -2,13 +2,14 @@ package ar.com.educacionit.domain;
 
 import ar.com.educacionit.services.CategoriaService;
 import ar.com.educacionit.services.SociosService;
+import ar.com.educacionit.services.exceptions.ServiceException;
 import ar.com.educacionit.services.impl.CategoriaServiceImpl;
 import ar.com.educacionit.services.impl.SociosServiceImpl;
 
 
 public class SociosTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ServiceException {
 		String nombre = "JUAN";
 		String apellido = "PEREZ";
 		String dni = "123456789";
